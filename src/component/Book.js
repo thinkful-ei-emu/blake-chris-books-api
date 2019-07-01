eba808477b263384fb59a7429f198f595b342829
Blake Lowrey <blake.lowrey1@gmail.com>
2019-07-01 16:50:33 -0400

    final functionality

--- src/component/Book.js
+++ src/component/Book.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
 function Book(props) {
+  //console.log(`salability :${props.thisbook.saleInfo.saleability}, amount : ${props.thisbook.saleInfo.listPrice.amount} ${props.thisbook.saleInfo.listPrice.currencyCode}`);
   return (
     <div>
         <img src={props.thisbook.volumeInfo.imageLinks.smallThumbnail} alt="book cover" />
-        <p>{props.thisbook.volumeInfo.title}</p>
+        <a href={props.thisbook.volumeInfo.infoLink}><p>{props.thisbook.volumeInfo.title}</p></a>
         <p>{props.thisbook.volumeInfo.authors && props.thisbook.volumeInfo.authors[0]}</p>
-        {/* <p>{props.thisbook.saleInfo.listprice['amount']}</p> */}
+        {(props.thisbook.saleInfo.saleability === 'FOR_SALE') && <p>{props.thisbook.saleInfo.listPrice.amount}{props.thisbook.saleInfo.listPrice.currencyCode}</p>}
         <p>{props.thisbook.volumeInfo.description}</p>
-    </div>);
-
+    </div>
+    );
 }
 
 export default Book;
--- src/component/BookList.js
+++ src/component/BookList.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Book from './Book'
 
 function BookList(props){
-    let newBooks = props.books.map(book =>
-        <li>
+    let newBooks = props.books.map((book, index)=>
+        <li key={index}>
             <Book thisbook={book} />
         </li>);
     return (
